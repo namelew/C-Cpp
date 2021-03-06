b8b4d983fa39eef579d8ca4130055808293ff824
namelew <71669420+namelew@users.noreply.github.com>
2021-03-05 23:44:07 -0300

    Teste

--- C/teste.c
+++ C/teste.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
- 
-int main() {
-    double salario, ajuste;
+#include <stdlib.h>
+
+int main(void){
+	void maior_e_menor(int array[], int tam);
+	int array[5] = {1, 7, 3, 2, 9};
+
+	maior_e_menor(array, 5);
+
+	return 0;
+}
+
+void maior_e_menor(int array[], int tam){
+	int maior, menor, i;
+
+	for(i = 0; i < tam; i++){
+		if(i == 0 || array[i] > maior){maior = array[i];}
+		if(i == 0 || array[i] < menor){menor = array[i];}
+	}
     
-    scanf("%lf", &salario);
-    
-    if(salario <= 400){
-        ajuste = 0.15;
-    }
-    if (salario <= 800 && salario > 400){
-        ajuste = 0.12;
-    }
-    if(salario <= 1200 && salario > 800){
-        ajuste = 0.10;
-    }
-    if(salario <= 2000 && salario > 1200){
-        ajuste = 0.07;
-    }
-    if(salario > 2000){
-        ajuste = 0.04;
-    }
-    
-    printf("Novo salario: %.2lf\n", salario * (1.0+ajuste));
-    printf("Reajuste ganho: %.2lf\n", salario * ajuste);
-    printf("Em percentual: %.0lf %% \n", ajuste * 100);
-    
-    return 0;
+	printf("Maior: %i\nMenor: %i\n", maior, menor);
 }
