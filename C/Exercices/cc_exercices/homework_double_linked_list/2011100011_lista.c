355ce0b90fa7f7449424dc709ee816fde607e1d5
namelew <71669420+namelew@users.noreply.github.com>
2021-04-25 00:23:55 -0300

    Finishing Funtions Homework CC

--- C/Exercices/cc_exercices/homework_double_linked_list/2011100011_lista.c
+++ C/Exercices/cc_exercices/homework_double_linked_list/2011100011_lista.c
@@ -2,6 +2,7 @@
 # include <stdlib.h>
 # include <string.h>
 
+// Falta testar nos casos de teste padrões
 typedef struct{
     int day, month, year;
 }Date;
@@ -23,8 +24,9 @@ typedef struct{
 void *insert_student(List *list, char prev_enroll[]);
 void *remove_student(List *list, char enrollment[]);
 void list_head(Student *head);
-void list_tail(Student *tail);
+void list_tail(List *list);
 void end(List *list);
+void get_last(List *list);
 
 int main(){
     List list;
@@ -52,7 +54,7 @@ int main(){
             list_head(list.head);
             break;
         case 4:
-            list_tail(list.tail);
+            list_tail(&list);
             break;
         default:
             break;
@@ -63,6 +65,7 @@ int main(){
 }
 
 void *insert_student(List *list, char prev_enroll[]){
+    // Ta tranquilo
     Student *aux;
     Student *pt = malloc(sizeof(Student));
     pt->next = NULL;
@@ -77,33 +80,98 @@ void *insert_student(List *list, char prev_enroll[]){
         list->head = pt;
         list->tail = pt;
     }else{
-        for(aux = list->head; aux != NULL; aux= aux ->next){
+        for(aux = list->head; aux!=NULL; aux=aux->next){
             if(!strcmp(prev_enroll, aux->enrollment)){
                 break;
             }
         }
-        // se aux->next = NULL, da overflow
-        aux->next->prev=pt;
-        pt->next = aux->next;
-        aux->next = pt;
-        pt->prev = aux; 
+        if(aux == NULL){
+            pt->next = list->head;
+            list->head->prev = pt;
+            list->head = pt;
+        }else{
+            if(aux->next == NULL && aux != NULL){
+                pt->next = aux->next;
+                aux->next = pt;
+                pt->prev = aux;
+            }else{
+                aux->next->prev=pt;
+                pt->next = aux->next;
+                aux->next = pt;
+                pt->prev = aux;
+            }
+        }
     }
-
 }
 
 void *remove_student(List *list, char enrollment[]){
-    Student *aux;
+    // Ta tranquilo
+    Student *aux = list->head;
+    while(aux != NULL){
+        if(strcmp(enrollment, aux->enrollment) == 0){break;}
+        aux = aux->next;
+    }
+    if(aux == list->head){
+        aux->next->prev = NULL;
+        list->head = aux->next;
+        free(aux);
+        return;
+    }
+    get_last(list);
+    if(aux == list->tail){
+        aux->prev->next = NULL;
+        list->tail = aux->prev;
+        free(aux);
+        return;
+    }
+    aux->prev->next = aux->next;
+    aux->next->prev = aux->prev;
+    free(aux);
 }
 
 void list_head(Student *head){
+    // Ta tranquilo
     Student *aux;
-
+    for(aux = head; aux != NULL; aux= aux->next){
+        printf("%s, ", aux->enrollment);
+        printf("%s, ", aux->name);
+        printf("%d/%d/%d, ",aux->birth_day.day, aux->birth_day.month, aux->birth_day.year);
+        printf("%.2f\n", aux->gpa);
+    }
 }
 
-void list_tail(Student *tail){
+void list_tail(List *list){
+    // Ta tranquilo
+    get_last(list);
     Student *aux;
+    for(aux = list->tail; aux != NULL; aux= aux->prev){
+        printf("%s, ", aux->enrollment);
+        printf("%s, ", aux->name);
+        printf("%d/%d/%d, ",aux->birth_day.day, aux->birth_day.month, aux->birth_day.year);
+        printf("%.2f\n", aux->gpa);
+    }
 }
 
 void end(List *list){
-    Student *aux;
+    // Ta tranquilo
+    Student *aux, *prev;
+    aux = list->head;
+    while(aux != NULL){
+        prev = aux;
+        aux = aux->next;
+        free(prev);
+        printf("*");
+    }
+    printf("\n");
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+void get_last(List *list){
+    // A principio funciona
+    Student *aux = list->head;
+    while(aux->next != NULL){
+        aux = aux->next;
+    }
+    list->tail = aux;
 }
