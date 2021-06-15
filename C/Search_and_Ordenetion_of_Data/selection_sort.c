fbad2fd89a35440906b6a2fa43307c96dcb5bf58
Diogo Cunha <71669420+namelew@users.noreply.github.com>
2021-06-15 11:25:32 -0300

    errata

--- C/Search_and_Ordenetion_of_Data/selection_sort.c
+++ C/Search_and_Ordenetion_of_Data/selection_sort.c
@@ -7,7 +7,7 @@ int main(){
     int array[] = {7, 10, 5, 3, 8, 4, 2, 9, 6};
     int i, tam = sizeof(array)/sizeof(int);
 
-    selection_sort(array, tam);
+    selection_sort(array,tam);
 
     for(i = 0; i < tam; i++){
         printf("A[%i] = %i\n", i, array[i]);
@@ -16,23 +16,23 @@ int main(){
     return 0;
 }
 
-void troca(int *array, int menor, int i){
+void troca(int *array, int posmenor, int i){
     int temp = array[i];
-    array[i] = array[menor];
-    array[menor] = temp;
+    array[i] = array[posmenor];
+    array[posmenor] = temp;
 }
 
 void selection_sort(int *array, int tam){
-    int i, menor, j;
+    int i, posmenor, j;
 
     for(i = 0; i < tam-1; i++){
-        menor = i;
+        posmenor = i;
         for(j = i+1; j < tam; j++){
-            if(array[menor] > array[j]){
-                menor = j;
+            if(array[posmenor] > array[j]){
+                posmenor = j;
             }
         }
-        troca(array, menor, i);
+        troca(array, posmenor, i);
     }
 
 }
