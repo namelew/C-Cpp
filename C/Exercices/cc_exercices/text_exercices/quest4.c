e15f14f0704c055891fb25e8564b2b66d38142fd
Diogo Cunha <71669420+namelew@users.noreply.github.com>
2021-08-10 17:36:33 -0300

    finishing the homework

--- C/Exercices/cc_exercices/text_exercices/quest4.c
+++ C/Exercices/cc_exercices/text_exercices/quest4.c
@@ -2,12 +2,37 @@
 # include <stdio.h>
 
 typedef struct{
-    char nome[40];
+    char nome[50];
     float n1;
     float n2;
 }Aluno;
 
 int main(){
-    
+    FILE *arq = fopen("alunos.txt", "r");
+    int i, tam;
+    Aluno a[7];
+
+    fscanf(arq, "%d\n", &tam);
+
+    for(i = 0; i < tam; i++){
+        fscanf(arq, "%[^\n]", &a[i].nome);
+        fscanf(arq, "%f\n", &a[i].n1);
+        fscanf(arq, "%f\n", &a[i].n2);
+    }
+    fclose(arq);
+
+    arq = fopen("alunos_binario.txt", "wb");
+    fwrite(a, sizeof(Aluno), tam, arq);
+    fclose(arq);
+
+    Aluno ba[tam];
+
+    arq = fopen("alunos_binario.txt", "rb");
+    fread(ba, sizeof(Aluno), tam, arq);
+
+    for(i = 0; i < tam; i++){
+        printf("Nome: %s - Nota 1: %.2f - Nota 2: %.2f\n", ba[i].nome, ba[i].n1, ba[i].n2);
+    }
+    fclose(arq);
     return 0;
 }
