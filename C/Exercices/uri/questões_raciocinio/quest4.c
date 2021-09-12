66dc67850c95fb1f6fc58ba896aff5748d83c7d4
Diogo Cunha <71669420+namelew@users.noreply.github.com>
2021-09-12 15:34:58 -0300

    correção da correção

--- C/Exercices/uri/questões_raciocinio/quest4.c
+++ C/Exercices/uri/questões_raciocinio/quest4.c
@@ -12,19 +12,19 @@ int main(){
     scanf("%d", &np);
 
     tpagar = conta/np;
-    printf("Valor a pagar por pessoa: %.2f\n", tpagar);
+    //printf("Valor a pagar por pessoa: %.2f\n", tpagar);
 
     //Desafio:
-    //double p[np], st = 0;
-    //for(i = 0; i < np; i++){
-    //    p[i] = tpagar;
-    //    st += tpagar;
-    //}
-    //p[i-1] = tpagar + ((st + (conta * 0.10)) - conta);
+    double p[np], st = 0;
+    for(i = 0; i < np; i++){
+        p[i] = tpagar;
+        st += tpagar;
+    }
+    p[i-1] = tpagar + ((st + (conta * 0.001)) - conta);
 
-    //for(i = 0; i < np; i++){
-    //    printf("Valor a pagar pessoa %d: %.2lf\n", i+1 , p[i]);
-    //}
+    for(i = 0; i < np; i++){
+        printf("Valor a pagar pessoa %d: %.2lf\n", i+1 , p[i]);
+    }
 
 
     return 0;
